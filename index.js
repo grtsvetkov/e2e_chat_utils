f3c37d6d8a3533723d7e35fc2bb08c39ba1df0dc
gr.tsvetkov <gr.tsvetkov@gmail.com>
2018-08-16 17:37:04 +0300

    1.0

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ class Client_e2e_Class {
         this.crypt = new JSEncrypt_LIB.JSEncrypt({default_key_size: this.key_size});
         this.cutMessageBy = this.key_size / 8; //256 chars
 
+
         this.storage_name = storage_name;
         this.initStorage();
 
@@ -17,17 +18,15 @@ class Client_e2e_Class {
     }
 
     initStorage() {
-        if (this.storage == false) {
-            let storage = localStorage.getItem(this.storage_name + '_' + this.user_id);
+        let storage = localStorage.getItem(this.storage_name + '_' + this.user_id) || '{}';
 
-            try {
-                this.storage = JSON.parse(storage);
-            } catch (err) {
+        try {
+            this.storage = JSON.parse(storage);
+        } catch (err) {
 
-                this.display('Хранилище ключей шифрования не нейдено. Создание нового хранилища');
+            this.display('Хранилище ключей шифрования не нейдено. Создание нового хранилища');
 
-                this.storage = {};
-            }
+            this.storage = {};
         }
     }
 
